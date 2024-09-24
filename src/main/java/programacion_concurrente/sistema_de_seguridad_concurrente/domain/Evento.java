278e3d38a733e1e39eb4acc80dc3506ab02b2911
marcos <marcgabe@myuax.com>
2024-09-24 18:20:53 +0200

    Apaños vitales (Evento) para (EventoService)
    Se readapta el codigo de Evento (domain) pregenerado por la version estipulada en bootify para porde hacer lo mismo propuesto en el commit previo

--- src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/domain/Evento.java
+++ src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/domain/Evento.java
@@ -2,7 +2,6 @@ package programacion_concurrente.sistema_de_seguridad_concurrente.domain;
 
 import jakarta.persistence.*;
 import java.time.OffsetDateTime;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +12,36 @@ import lombok.Setter;
 @Setter
 public class Evento {
 
-    private String NivelCriticidad;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEvento;
+
+    @Column(nullable = false)
+    private String nivelCriticidad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "eventos_id")
+    private SensorAcceso eventos;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "eventoss_id")
+    private SensorMovimiento eventoss;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "eventosss_id")
+    private SensorTemperatura eventosss;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sensor_temperatura_id")
+    private SensorTemperatura sensorTemperatura;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sensor_movimiento_id")
+    private SensorMovimiento sensorMovimiento;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sensor_acceso_id")
+    private SensorAcceso sensorAcceso;
 
     @Column(nullable = false)
     private OffsetDateTime fechaHora;
