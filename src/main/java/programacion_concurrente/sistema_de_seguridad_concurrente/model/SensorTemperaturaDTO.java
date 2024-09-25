71894f954819fdf808b5aeed7d59c69eb530f4a9
marcos <marcgabe@myuax.com>
2024-09-25 22:03:40 +0200

    SensorTempDTO
    mejor entendimiento de referencia incluimos aspectos no presentados previamente

--- src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/model/SensorTemperaturaDTO.java
+++ src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/model/SensorTemperaturaDTO.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import programacion_concurrente.sistema_de_seguridad_concurrente.domain.Evento;
+
+import java.util.Set;
 
 
 @Getter
@@ -21,6 +24,10 @@ public class SensorTemperaturaDTO {
 
     @NotNull
     private Integer temperatura;
+
+    @NotNull
+    private Set<Evento> sensorTemperatura;
+
 
     @NotNull
     private Integer sensorees;
